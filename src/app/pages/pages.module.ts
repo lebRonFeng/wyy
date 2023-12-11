0d0eaf87eb876718756a2bb255058f68dd686ea4
liu_huifeng <10640370+feng88888@user.noreply.gitee.com>
2023-12-11 15:36:48 +0800

    模块化设计，首页基础搭建

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { ShareModule } from '../share/share.module'
+@NgModule({
+  declarations: [],
+  imports: [
+    ShareModule
+  ]
+})
+export class PagesModule { }
